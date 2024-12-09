5a99c7f19f6639140dd575f93b0fbccf1b73a713
Oscare101 <kirillfedortsev2000@gmail.com>
2024-12-09 11:55:53 +0200

    enPassant cell attack circle visual

--- src/components/chess/RenderCellItem.tsx
+++ src/components/chess/RenderCellItem.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 import rules from '../../constants/rules';
 import Icon from '../icons/Icon';
 import colors from '../../constants/colors';
-import {GameStatInterface} from '../../constants/interfaces';
+import {
+  GameStatInterface,
+  PiecePlacementLogType,
+} from '../../constants/interfaces';
 
 const width = Dimensions.get('screen').width;
 
@@ -19,46 +22,45 @@ function RenderCellItem(props: {
     (props.column.index % 2 === 0 && props.row.index % 2 === 0) ||
     (props.column.index % 2 === 1 && props.row.index % 2 === 1);
 
-  const cell =
+  const lastPiecePlacement: PiecePlacementLogType =
     props.gameStat.piecesPlacementLog[
       props.gameStat.piecesPlacementLog.length - 1
-    ][cellIndex];
+    ];
+  const cell = lastPiecePlacement[cellIndex];
   const piece = cell?.piece;
-
   const lastMove =
     props.gameStat.movesHistory[props.gameStat.movesHistory.length - 1];
+  const activeCell = props.gameStat.activeCell;
 
-  const cellStyle: ViewStyle = React.useMemo(
-    () => ({
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: width * 0.12,
-      aspectRatio: 1,
-      backgroundColor:
-        lastMove?.from === cellIndex
-          ? isEven
-            ? colors.piecePointBlack
-            : colors.piecePointWhite
-          : lastMove?.to === cellIndex
-          ? isEven
-            ? colors.piecePointBlack
-            : colors.piecePointWhite
-          : props.gameStat.activeCell === cellIndex
-          ? isEven
-            ? colors.piecePointBlack
-            : colors.piecePointWhite
-          : isEven
-          ? colors.cellBlack
-          : colors.cellWhite,
-      transform: props.gameStat.step === 'black' ? [{rotate: '180deg'}] : [],
-    }),
-    [
-      cellIndex,
-      lastMove,
-      props.gameStat.activeCell,
-      props.gameStat.step,
-      isEven,
-    ],
+  const cellStyle: ViewStyle = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width * 0.12,
+    aspectRatio: 1,
+    backgroundColor:
+      lastMove?.from === cellIndex
+        ? isEven
+          ? colors.piecePointBlack
+          : colors.piecePointWhite
+        : lastMove?.to === cellIndex
+        ? isEven
+          ? colors.piecePointBlack
+          : colors.piecePointWhite
+        : activeCell === cellIndex
+        ? isEven
+          ? colors.piecePointBlack
+          : colors.piecePointWhite
+        : isEven
+        ? colors.cellBlack
+        : colors.cellWhite,
+    transform: props.gameStat.step === 'black' ? [{rotate: '180deg'}] : [],
+  };
+
+  const enPassantCell: boolean = !!(
+    activeCell &&
+    activeCell % 8 !== cellIndex % 8 &&
+    lastPiecePlacement[activeCell].piece?.name === 'Pawn' &&
+    lastPiecePlacement[cellIndex].status === 'free'
   );
 
   return (
@@ -67,19 +69,7 @@ function RenderCellItem(props: {
       disabled={!!props.gameStat.gameResult || !props.gameStat.isGameActive}
       style={cellStyle}>
       {props.gameStat.routeCells?.includes(cellIndex) ? (
-        props.gameStat.piecesPlacementLog[
-          props.gameStat.piecesPlacementLog.length - 1
-        ][cellIndex].status === 'free' ? (
-          <View
-            style={{
-              width: '40%',
-              aspectRatio: 1,
-              borderRadius: 100,
-              backgroundColor: colors.cellPoint,
-              position: 'absolute',
-            }}
-          />
-        ) : (
+        lastPiecePlacement[cellIndex].status === 'occupied' || enPassantCell ? (
           <View
             style={{
               width: '100%',
@@ -88,6 +78,16 @@ function RenderCellItem(props: {
               borderColor: colors.cellPoint,
               position: 'absolute',
               borderWidth: 5,
+            }}
+          />
+        ) : (
+          <View
+            style={{
+              width: '40%',
+              aspectRatio: 1,
+              borderRadius: 100,
+              backgroundColor: colors.cellPoint,
+              position: 'absolute',
             }}
           />
         )
